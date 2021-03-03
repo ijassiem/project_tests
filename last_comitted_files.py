b8d6961af6de68565440dfb7418e63ee6397e2d9
Ismail <56064610+ijassiem@users.noreply.github.com>
2021-03-03 08:07:39 +0200

    Update last_comitted_files.py

--- last_comitted_files.py
+++ last_comitted_files.py
@@ -4,7 +4,9 @@ out = subprocess.check_output('git show --name-only', shell=True)
 #out=out.split('\n')
 print '----------'
 print out
-#if 'bc8n54M32k' in out:
-#    print 'File bc8n54M32k was found.'
+if 'hi.py' in out:
+    print 'hi.py is found to be the last committed file.'
+else:
+  print 'hi.py not found.'
 #if 'bc8n856M1k' in out:
 #    print 'File bc8n856M1k was found.'
