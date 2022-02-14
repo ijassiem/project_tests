42ab2f9ed543d23eb3a30e1f6674e7dc1474302b
ismjas <99168728+ismjas@users.noreply.github.com>
2022-02-14 10:03:08 +0200

    Update hi.py

--- hi.py
+++ hi.py
@@ -9,5 +9,3 @@ sum = a + b + x + y + z
 product = a * b * x * y *z
 average = sum/5
 print("Hello world test")
-######################!!!!!!!!!!!!!!!!!!!!!#####################
-#branch feature-7. Testing restriction of untrusted forked PR builds on Jenksins.
